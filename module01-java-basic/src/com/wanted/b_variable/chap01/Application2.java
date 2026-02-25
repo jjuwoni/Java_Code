f1d01b4b82099bcb44a23718fcaa6cd024777bd7
jjuwoni <sjw4690@gmail.com>
2026-02-25 17:23:29 +0900

    day_01_02.25

---
+++ module01-java-basic/src/com/wanted/b_variable/chap01/Application2.java
@@ -0,0 +1,17 @@
+package com.wanted.b_variable.chap01;
+
+import java.util.Scanner;
+
+public class Application2 {
+    public static void main(String[] args) {
+        // Scanner 클래스는 사용자의 입력을 저장할 수 있다.
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter your name : ");
+        String name = sc.nextLine();
+
+        System.out.println("entered Name : " + name);
+
+        // sc.
+    }
+}
